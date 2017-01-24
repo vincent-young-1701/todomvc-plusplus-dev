307969c28a832611b12c35e7bd7672bbbb7826b5
Vincent Young <vincent_young@apple.com>
2017-01-23 20:58:05 -0800

    Init files for exercie 1

---
+++ index.js
@@ -0,0 +1,19 @@
+{
+  "name": "todomvc-plusplus-dev",
+  "version": "1.0.0",
+  "description": "https://github.com/kwhinnery/todomvc-plusplus",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/vincent-young-1701/todomvc-plusplus-dev.git"
+  },
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/vincent-young-1701/todomvc-plusplus-dev/issues"
+  },
+  "homepage": "https://github.com/vincent-young-1701/todomvc-plusplus-dev#readme"
+}
